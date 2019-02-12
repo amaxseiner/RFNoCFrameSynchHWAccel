0196e391c9b61acab6efaa92486f553f5749baae
Alex <amaxsein@masonlive.gmu.edu>
2019-02-11 22:52:46 -0500

    still working with logging in C sims

--- complexTinkering/complexGMUTB.cpp
+++ complexTinkering/complexGMUTB.cpp
@@ -1,27 +1,36 @@
 #include "complexGMU.h"
-
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
 using namespace std;
 
 int main(int argc, char **argv){
+	FILE *result;
 	rfnoc_axis streamArrayIn[256];
 	rfnoc_axis streamArrayOut[256];
 	generatePreamble();
 	rfnoc_axis temp;
 	rfnoc_axis temp2;
-
-	ap_ufixed<4,2> tester;
-	tester.V = 1.75;
-	ap_ufixed<4,2> testes;
-	testes.V = 2.5;
-	ap_ufixed<8,4> yeeter;
+	result = fopen("result.dat","wb");
+	ap_fixed<4,2> tester;
+	tester = 1.75;
+	ap_fixed<4,2> testes;
+	testes = 2.5;
+	ap_fixed<8,4> yeeter;
 	yeeter = tester * testes;
 
-	for(int i; i< 256;i++){
+	fwrite(tester,sizeof(ap_fixed<4,2>),result);
+	/*result << setw(10) << testes;
+	result << setw(10) << yeeter;
+	result << endl;
+	result.close();*/
+	/*for(int i; i< 256;i++){
 		ap_int<32> pos = i;
 		float q = ((16*(16+i)) + (1+i));
 		temp.data = ((16*(16+i)) + (1+i));
 		complexMultiplier(temp,&temp2,pos);
 
-	}
-
+	}*/
+	return 0;
 }
