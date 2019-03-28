bde4be4a4e193a5280467a01ea9a13d4c916ebac
damaximum <amaxsein@masonlive.gmu.edu>
2019-03-28 14:12:03 -0400

    updates to scirpts and logged data

--- scirpts/createGRCTestData.py
+++ scirpts/createGRCTestData.py
@@ -7,13 +7,13 @@ f = open("/home/alex/Documents/RFNoCFrameSynchHWAccel/scirpts/roundedDat.csv")
 data = []
 
 for line in f:
-    print(abs(int(line)))
+    data.append(line)
     data.append(line)
 
 datanp = np.asarray(data)
 
 datanp.astype(scipy.int16).tofile("testCorPower.log")
 
-f = scipy.fromfile(open("/home/alex/Documents/RFNoCFrameSynchHWAccel/scirpts/testCorPower.log"), dtype=scipy.complex16)
+f = scipy.fromfile(open("/home/alex/Documents/RFNoCFrameSynchHWAccel/scirpts/testCorPower.log"), dtype=scipy.int16)
 for line in f:
     print line
